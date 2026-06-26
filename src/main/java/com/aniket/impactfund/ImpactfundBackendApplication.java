7497e38c2e42187097a8795157d14ecfcfa556f9
Aniket_Borse <aniket100borse@gmail.com>
2026-06-26 23:06:05 +0530

    chore: initialize ImpactFund backend project structure

---
+++ src/main/java/com/aniket/impactfund/ImpactfundBackendApplication.java
@@ -0,0 +1,13 @@
+package com.aniket.impactfund;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImpactfundBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImpactfundBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aniket/impactfund/ImpactfundBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aniket.impactfund;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImpactfundBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
