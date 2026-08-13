3c752932b9b0750508abc1aa2be7155738cc9ca9
Aniket_Borse <aniket100borse@gmail.com>
2026-08-13 09:00:30 +0530

    feat(loan): loan creation service done

--- src/main/java/com/aniket/impactfund/loan/service/impl/LoanServiceImpl.java
+++ src/main/java/com/aniket/impactfund/loan/service/impl/LoanServiceImpl.java
@@ -1,4 +1,54 @@
 package com.aniket.impactfund.loan.service.impl;
 
-public class LoanServiceImpl {
+import com.aniket.impactfund.borrower.entity.Borrower;
+import com.aniket.impactfund.borrower.repository.BorrowerRepository;
+import com.aniket.impactfund.common.exception.ResourceNotFoundException;
+import com.aniket.impactfund.loan.dto.request.CreateLoanRequest;
+import com.aniket.impactfund.loan.dto.response.LoanResponse;
+import com.aniket.impactfund.loan.entity.Loan;
+import com.aniket.impactfund.loan.enums.LoanStatus;
+import com.aniket.impactfund.loan.mapper.LoanMapper;
+import com.aniket.impactfund.loan.repository.LoanRepository;
+import com.aniket.impactfund.loan.service.LoanService;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.UUID;
+
+public class LoanServiceImpl implements LoanService {
+    private LoanRepository loanRepository;
+    private BorrowerRepository borrowerRepository;
+    private LoanMapper loanMapper;
+
+    @Override
+    public LoanResponse createLoan(CreateLoanRequest request) {
+        Borrower borrower = borrowerRepository.findByUuid(request.borrowerUuid())
+                .orElseThrow(() ->
+                        new ResourceNotFoundException(
+                                "Borrower",
+                                "UUID",
+                                request.borrowerUuid()
+                        ));
+
+        Loan loan = loanMapper.toEntity(request, borrower);
+        loan.setApplicationDate(LocalDate.now());
+        loan.setStatus(LoanStatus.PENDING);
+        Loan savedLoan = loanRepository.save(loan);
+        return loanMapper.toResponse(savedLoan);
+    }
+
+    @Override
+    public LoanResponse getLoanByUuid(UUID uuid) {
+        return null;
+    }
+
+    @Override
+    public List<LoanResponse> getAllLoans() {
+        return List.of();
+    }
+
+    @Override
+    public List<LoanResponse> getLoansByBorrower(UUID borrowerUuid) {
+        return List.of();
+    }
 }
